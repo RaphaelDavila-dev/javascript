64685065680b874523cbf4d27e60f2ef6803e872
Raphael d'Avila <raphinha.350@gmail.com>
2025-07-29 21:01:59 -0300

    ex008,009009b e 010 feitos

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 90
+console.log(`A velociade do seu carro é ${vel}km/h`)
+if(vel > 60){
+    console.log('Voce ultrapassou o limite de velocidade. Multado!')
+}
+    console.log('Dirija sempre com o sinto de segurança')
