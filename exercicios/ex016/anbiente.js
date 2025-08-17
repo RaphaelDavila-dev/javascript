dac3740e117354ec01049995ff26004983ae3e25
Raphael d'Avila <raphinha.350@gmail.com>
2025-08-17 14:53:55 -0300

    ex016 e 017 criados

---
+++ exercicios/ex016/anbiente.js
@@ -0,0 +1,14 @@
+var c = 1
+for(var c =1; c<= 10; c++){
+    console.log('valor abaixo')
+}
+
+/*while(c<=10){
+    console.log('valor abaixo')
+    c++
+}*/
+
+/*do{
+    console.log(c)
+    c++
+}while(c<=10)*/
